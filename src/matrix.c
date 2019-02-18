b087cd8c6ca4b59d01379c8b5414d5ef67e23c01
Garrett Luu <garrettluu131@gmail.com>
2019-02-17 22:47:15 -0800

    Added matrix addition function

--- src/matrix.c
+++ src/matrix.c
@@ -34,7 +34,9 @@ transform_t *newTransform(float *elements) {
  * vector: vector to be transformed
  */
 void transformVector(vector_t *vector, transform_t transform) {
+  //Copy the original vector
   vector_t original = *vector;
+  //Matrix multiplication between 3x3 and 1x3 matrices
   vector->x = (original.x * (*(transform.matrix))) +
       (original.y * (*(transform.matrix + 1))) +
       (original.z * (*(transform.matrix + 2)));
@@ -44,11 +46,30 @@ void transformVector(vector_t *vector, transform_t transform) {
   vector->z = (original.x * (*(transform.matrix + 6))) +
       (original.y * (*(transform.matrix + 7))) +
       (original.z * (*(transform.matrix + 8)));
+  //Destroy the original vector
   free(&original);
 }
+//
+transform_t *compose(transform_t transform1, transform_t transform2) {
+  transform_t *result;
+  // *(result->matrix) = {*(transform1.matrix) * *(transform2.matrix) + *(&(transform1.matrix) + 3)
 
-transform_t *compose(transform_t t1, transform_t t2) {
-  transform_t *result = (transform_t *) malloc(sizeof(transform_t));
-  //TODO: matrix multiplication algorithm
+  // TODO: matrix multiplication algorithm
   return result;
+}
+
+transform_t *addMatrices(transform_t transform1, transform_t transform2) {
+  transform_t result;
+  *(result.matrix) = *(transform1.matrix) + *(transform2.matrix);
+  *(result.matrix + 1) = *(transform1.matrix + 1) + *(transform2.matrix + 1);
+  *(result.matrix + 2) = *(transform1.matrix + 2) + *(transform2.matrix + 2);
+
+  *(result.matrix + 3) = *(transform1.matrix + 3) + *(transform2.matrix + 3);
+  *(result.matrix + 4) = *(transform1.matrix + 4) + *(transform2.matrix + 4);
+  *(result.matrix + 5) = *(transform1.matrix + 5) + *(transform2.matrix + 5);
+
+  *(result.matrix + 6) = *(transform1.matrix + 6) + *(transform2.matrix + 6);
+  *(result.matrix + 7) = *(transform1.matrix + 7) + *(transform2.matrix + 7);
+  *(result.matrix + 8) = *(transform1.matrix + 8) + *(transform2.matrix + 8);
+  return &result;
 }
--- src/matrix.h
+++ src/matrix.h
@@ -8,6 +8,9 @@ typedef struct transformation {
 transform_t *newTransform(float *elements);
 
 void transformVector(vector_t *vector, transform_t transform);
-transform_t *compose(transform_t t1, transform_t t2);
+
+transform_t *compose(transform_t transform1, transform_t transform2);
+
+transform_t *addMatrices(transform_t transform1, transform_t transform2);
 
 #endif
