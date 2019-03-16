9b94c126409c67bd9e09c28db427b21cfd3118ad
Garrett Luu <garrettluu131@gmail.com>
2019-03-16 15:51:23 -0700

    Created empty functions for parser

---
+++ src/parser.c
@@ -0,0 +1,9 @@
+#include "parser.h"
+
+char *shunt(char *infix) {
+
+}
+
+double parse(char *rpn) {
+
+}
---
+++ src/parser.h
@@ -0,0 +1,8 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+char *shunt(char *infix);
+
+double parse(char *rpn);
+
+#endif
