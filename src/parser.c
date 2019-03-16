e8f934a09ed8142034e19007aa6c6292d47052a3
Garrett Luu <garrettluu131@gmail.com>
2019-03-16 16:12:11 -0700

    Added stack for RPN parser

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,23 @@
 #include "parser.h"
 
+RPNStack_t *makeRPNStack(uint8_t capacity) {
+  RPNStack_t *stack = malloc(sizeof(RPNStack_t));
+  stack->elements = malloc(capacity * sizeof(double));
+  stack->capacity = capacity;
+  stack->size = 0;
+  return stack;
+}
+
+void pushRPN(RPNStack_t *stack, double value) {
+  stack->elements[stack->size] = value;
+  stack->size++;
+}
+
+double popRPN(RPNStack_t *stack) {
+  stack->size--;
+  return stack->elements[stack->size];
+}
+
 char *shunt(char *infix) {
 
 }
--- src/parser.h
+++ src/parser.h
@@ -1,8 +1,17 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-char *shunt(char *infix);
+typedef struct RPNStack {
+  double *elements;
+  uint8_t size;
+  uint8_t capacity;
+} RPNStack_t;
 
+RPNStack_t *makeRPNStack(uint8_t capacity);
+void pushRPN(RPNStack_t *stack, double value);
+double popRPN(RPNStack_t *stack);
+
+char *shunt(char *infix);
 double parse(char *rpn);
 
 #endif
