60c3f410fcddbc79401a9728069473a32cae3b18
Garrett Luu <garrettluu131@gmail.com>
2019-06-04 21:11:11 -0700

    Fixed memory allocation issues

--- src/vector.c
+++ src/vector.c
@@ -22,7 +22,7 @@
 #include "vector.h"
 
 vector_t *newVector(float xCoord, float yCoord, float zCoord) {
-  vector_t *vector = (vector_t *) malloc(sizeof(vector_t));
+  vector_t *vector = malloc(sizeof(vector_t));
   vector->x = xCoord;
   vector->y = yCoord;
   vector->z = zCoord;
@@ -30,7 +30,7 @@ vector_t *newVector(float xCoord, float yCoord, float zCoord) {
 }
 
 vector_t *newVectorFromArray(float *components) {
-  vector_t *vector = (vector_t *) malloc(sizeof(vector_t));
+  vector_t *vector = malloc(sizeof(vector_t));
   vector->x = *components;
   vector->y = *(components + 1);
   vector->z = *(components + 2);
@@ -38,11 +38,8 @@ vector_t *newVectorFromArray(float *components) {
 }
 
 vector_t *add(vector_t v1, vector_t v2) {
-  vector_t sum;
-  sum.x = v1.x + v2.x;
-  sum.y = v1.y + v2.y;
-  sum.z = v1.z + v2.z;
-  return &sum;
+  vector_t *sum = newVector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+  return sum;
 }
 
 vector_t *subtract(vector_t v1, vector_t v2) {
@@ -54,10 +51,10 @@ vector_t *subtract(vector_t v1, vector_t v2) {
 }
 
 vector_t *scale(vector_t v1, float scaleFactor) {
-  vector_t *scaled = malloc(sizeof(vector_t));
-  scaled->x = v1.x * scaleFactor;
-  scaled->y = v1.y * scaleFactor;
-  scaled->z = v1.z * scaleFactor;
+  vector_t *scaled = newVector(
+      v1.x * scaleFactor,
+      v1.y * scaleFactor,
+      v1.z * scaleFactor);
   return scaled;
 }
 
@@ -66,20 +63,20 @@ double dot(vector_t v1, vector_t v2) {
 }
 
 vector_t *cross(vector_t v1, vector_t v2) {
-  vector_t cross;
-  cross.x = ((v1.y * v2.z) - (v2.y * v1.z));
-  cross.y = -((v1.x * v2.z) - (v2.x * v1.z));
-  cross.z = (v1.x * v2.y) - (v2.x * v1.y);
-  return &cross;
+  vector_t *cross = newVector(
+      ((v1.y * v2.z) - (v2.y * v1.z)),
+      -((v1.x * v2.z) - (v2.x * v1.z)),
+      (v1.x * v2.y) - (v2.x * v1.y));
+  return cross;
 }
 
 vector_t *normalize(vector_t vector) {
   float norm = length(vector);
-  vector_t unit;
-  unit.x = vector.x / norm;
-  unit.y = vector.y / norm;
-  unit.z = vector.z / norm;
-  return &unit;
+  vector_t *unit = newVector(
+      vector.x / norm,
+      vector.y / norm,
+      vector.z / norm);
+  return unit;
 }
 
 float length(vector_t vector) {
@@ -90,6 +87,6 @@ float length(vector_t vector) {
 float distance(vector_t v1, vector_t v2) {
   vector_t *diff = subtract(v1, v2);
   float result = length(*diff);
-  free(&diff);
+  free(diff);
   return result;
 }
